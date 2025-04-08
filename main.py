a52f8605c7ad356345b19e1e8abc5d7976f9b699
Alex <killerpro02@mail.ru>
2025-04-08 21:30:48 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import uvicorn
-from fastapi import FastAPI, HTTPException, status, Request
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from mail.database.db_session import create_tables_mail
-from mail.api_mail import api_v1
+from mail.api.api_mail import api_v1
 from loguru import logger
 from mail.database.db_session import async_db_mail
-from mail.imap_smtp_connect.imap_connection import IMAPPool
 from mail.schemas.tags_api import tags_metadata
 
 
@@ -53,7 +52,7 @@ app.add_middleware(
 
 
 @app.exception_handler(HTTPException)
-async def custom_http_exception_handler(exc: HTTPException):
+async def custom_http_exception_handler(request: Request, exc: HTTPException):
     return JSONResponse(
         status_code=exc.status_code,
         content={"message": exc.detail}
