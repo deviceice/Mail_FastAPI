6a3a991f891c3babf03c1e78a6fdf065aa7d8593
Alex <killerpro02@mail.ru>
2025-04-09 13:14:45 +0300

    fix sqlalchemy async session

--- app/mail/api/api_mail.py
+++ app/mail/api/api_mail.py
@@ -1,11 +1,15 @@
 import asyncio
+import email
 
 from fastapi import (APIRouter, HTTPException, Response, BackgroundTasks, Depends, Query)
 from fastapi.responses import StreamingResponse
+from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status as status_code
 
 from mail.imap_smtp_connect.imap_connection import get_imap_connection
 from mail.imap_smtp_connect.smtp_connection import get_smtp_connection
+from mail.database.db_session import get_session
+from mail.database.crud_mail import *
 from mail.support_func_API import *
 from mail.schemas.request.schemas_mail import *
 from mail.schemas.response.schemas_mail import *
@@ -14,7 +18,6 @@ from mail.example_schemas.response_schemas_examples import *
 from mail.example_schemas.request_schemas_examples import *
 from mail.options_emails import EmailFlags
 from mail.http_exception.default_exception import HttpExceptionMail
-import email
 
 api_v1 = APIRouter(prefix="/api/v1")
 
@@ -32,9 +35,11 @@ async def emails(
                                      ge=1, le=500),
         last_uid: Optional[str] = Query(None,
                                         description="Последний UID письма, после которого прислать следующие письма"),
-        imap=Depends(get_imap_connection)):
+        imap=Depends(get_imap_connection)):  # session: AsyncSession = Depends(get_session)
+
     # ТЕСТИРОВАНИЕ
     # settings: Settings = Depends(get_settings) Асинхронное использование Settings
+
     try:
         status, response = await imap.list('""', '"*"')
     except asyncio.exceptions.TimeoutError:
--- app/mail/database/crud_mail.py
+++ app/mail/database/crud_mail.py
@@ -1,1 +1,12 @@
 # Запросы в БД на этапе разработки
+from app.mail.database.models_mail import *
+
+
+# Test корректной работы сессии
+async def db_test(session):
+    async with session as s:
+        check = await s.execute(
+            select(Users))
+        check = check.scalar()
+        # print(check.id, check.login, check.password)
+    return check
